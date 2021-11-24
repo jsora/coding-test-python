4bec611b11d068684dfe542b9558d3c11760c827
Juan Pablo <jsora@outlook.com>
2021-11-24 03:29:47 -0500

    Se actualiza el archivo main.py del paquete test, condicionamdo el retorno exitoso o fallido del consumo del servicio, se almacena el tag descripcion en una variable para guardarla en el archivo joker.txt, se cierra el archivo

--- test/main.py
+++ test/main.py
@@ -7,6 +7,12 @@ import requests
 
 def run():
     r = requests.get('https://api.chucknorris.io/jokes/random')
-    with open('json-joker.json', 'w') as dataFile:
-        json.dump(r.json(), dataFile, indent=4)
-    return 1
+    if(r.status_code == 200):
+        data = r.json()
+        descripcion = data['value']
+        with open('joker.txt', 'w') as dataFile:
+            json.dump(descripcion, dataFile, indent=4)
+        dataFile.close()
+        return 1
+    else:
+        return 0
